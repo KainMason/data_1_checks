5e8852adf5e276cb81adb91509d79a9bd309f4dd
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-02-09 02:18:50 -0500

    Completed the requirements of the KnowledgeCheck

---
+++ knowlegeCheck01.py
@@ -0,0 +1,17 @@
+# Printing a string
+print("Hello World!")
+
+# Creating a list and printing one of its values
+star_trek_ships = ["Enterprise", "Voyager", "Defiant", "Discovery", "Shenzhou"]
+print(star_trek_ships[2])
+
+# Creating a dictionary and printing one of its values
+star_trek_captains = {
+    "Kirk": "James T. Kirk",
+    "Picard": "Jean-Luc Picard"
+}
+print(star_trek_captains["Picard"])
+
+# Creating a tuple and printing one of its values
+star_trek_series = ("The Original Series", "The Next Generation", "Deep Space Nine", "Voyager")
+print(star_trek_series[1])
