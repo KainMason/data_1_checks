1cb6485ea68e1e768aafdccd89e8bda7e718f3b3
Tyler Kain Mason <95332889+KainMason@users.noreply.github.com>
2023-03-03 17:29:01 -0500

    Completed KC_2 assignment

--- kc_2.py
+++ kc_2.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# read the CSV file into a pandas dataframe
+df = pd.read_csv("assets/hogwarts.csv")
+
+# count the number of positive and negative feedback
+feedback_count = df['Feedback'].value_counts()
+
+# create a pie chart to show the percentage of positive and negative feedback
+plt.pie(feedback_count, labels=feedback_count.index, autopct='%1.1f%%')
+plt.title("Hogwarts Legacies: Positive vs Negative Feedback")
+plt.show()
